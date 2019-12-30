38bdcef7dd26bf4bc6e6c03eb8e70ff03d6bd455
Renato Pinto <renatopl@Renatos-MacBook-Pro.local>
2019-12-30 20:32:13 +0100

    added store and keeping searched recipes in recipes view

--- src/views/Recipes/Recipes.js
+++ src/views/Recipes/Recipes.js
@@ -1,4 +1,5 @@
 import modelInstance from "../../data/DataModel"
+import Store from '../../data/Store';
 
 import React, { Component } from "react";
 import {Link} from "react-router-dom";
@@ -19,119 +20,29 @@ class Recipes extends Component {
 	constructor(props) {
 		super(props);
         this.state = {
-            "recipes": [
-                {
-                    "id": 592479,
-                    "title": "Kale and Quinoa Salad with Black Beans",
-                    "readyInMinutes": 50,
-                    "servings": 6,
-                    "image": "Kale-and-Quinoa-Salad-with-Black-Beans-592479.jpg",
-                    "imageUrls": [
-                        "Kale-and-Quinoa-Salad-with-Black-Beans-592479.jpg"
-                    ]
-                },
-                {
-                    "id": 547775,
-                    "title": "Creamy Avocado Pasta",
-                    "readyInMinutes": 15,
-                    "servings": 2,
-                    "image": "Creamy-Avocado-Pasta-547775.jpg",
-                    "imageUrls": [
-                        "Creamy-Avocado-Pasta-547775.jpg"
-                    ]
-                },
-                {
-                    "id": 818941,
-                    "title": "Avocado Toast with Eggs, Spinach, and Tomatoes",
-                    "readyInMinutes": 10,
-                    "servings": 1,
-                    "image": "avocado-toast-with-eggs-spinach-and-tomatoes-818941.jpg",
-                    "imageUrls": [
-                        "avocado-toast-with-eggs-spinach-and-tomatoes-818941.jpg"
-                    ]
-                },
-                {
-                    "id": 495111,
-                    "title": "Citrus Sesame Kale",
-                    "readyInMinutes": 15,
-                    "servings": 4,
-                    "image": "Citrus-Sesame-Kale-495111.jpg",
-                    "imageUrls": [
-                        "Citrus-Sesame-Kale-495111.jpg"
-                    ]
-                },
-                {
-                    "id": 689502,
-                    "title": "Melt In Your Mouth Kale Salad",
-                    "readyInMinutes": 10,
-                    "servings": 2,
-                    "image": "Melt-In-Your-Mouth-Kale-Salad-689502.jpg",
-                    "imageUrls": [
-                        "Melt-In-Your-Mouth-Kale-Salad-689502.jpg"
-                    ]
-                },
-                {
-                    "id": 837136,
-                    "title": "Kale Pineapple Smoothie",
-                    "readyInMinutes": 4,
-                    "servings": 1,
-                    "image": "kale-pineapple-smoothie-837136.jpg",
-                    "imageUrls": [
-                        "kale-pineapple-smoothie-837136.jpg"
-                    ]
-                },
-                {
-                    "id": 582897,
-                    "title": "Mexican Salad with Lime Dressing",
-                    "readyInMinutes": 15,
-                    "servings": 4,
-                    "image": "Mexican-Salad-with-Lime-Dressing-582897.jpg",
-                    "imageUrls": [
-                        "Mexican-Salad-with-Lime-Dressing-582897.jpg"
-                    ]
-                },
-                {
-                    "id": 777037,
-                    "title": "Weekly Meal Plan #17",
-                    "readyInMinutes": 15,
-                    "servings": 6,
-                    "image": "weekly-meal-plan-17-777037.jpg",
-                    "imageUrls": [
-                        "weekly-meal-plan-17-777037.jpg"
-                    ]
-                },
-                {
-                    "id": 801710,
-                    "title": "Matcha Green Tea and Pineapple Smoothie",
-                    "readyInMinutes": 10,
-                    "servings": 1,
-                    "image": "matcha-green-tea-and-pineapple-smoothie-801710.jpg",
-                    "imageUrls": [
-                        "matcha-green-tea-and-pineapple-smoothie-801710.jpg"
-                    ]
-                }
-            ],
-            "total": 313346,
-            "baseURI": "https://spoonacular.com/recipeImages/"
+            "recipes": Store.searchedRecipes || [],
+            "total": null,
+            "baseURI": null
         }
     }
-    /*
+
 	componentDidMount() {
 		modelInstance.getRecipes(limit, offset)
 			.then(data => {
                 this.setState({
-                    recipes: data.results,
-                    total: data.totalResults,
-					baseURI: data.baseUri
+                    recipes: (this.state.recipes.length === 0 ? data.results : this.state.recipes),
+                    total: (this.state.total === null ? data.totalResults : this.state.total),
+                    baseURI: (this.state.baseURI === null ? data.baseUri : this.state.baseURI),
                 });
+                if (Store.searchedRecipes === null)
+                    Store.searchedRecipes = this.state.recipes;
 			}).catch(error => {
                 console.error(error);
             });
     }
-    */
 
     getMoreRecipes = () =>{
-        offset += limit;
+        offset += (this.state.recipes.length + 1);
 
         modelInstance.getRecipes(limit, offset)
 			.then(data => {
@@ -139,8 +50,9 @@ class Recipes extends Component {
                 const old_recipes = this.state.recipes.slice();
                 this.setState({
                     recipes: old_recipes.concat(data.results),
-                    total: data.totalResults,
                 });
+                offset = 0;
+                Store.searchedRecipes = this.state.recipes;
 			}).catch(error => {
                 console.error(error);
             });
