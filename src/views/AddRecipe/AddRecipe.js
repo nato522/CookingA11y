437b34921fc4239074f2b54350cf46f9eebeb73c
Renato Pinto <renatopl@Renatos-MacBook-Pro.local>
2020-01-04 19:59:02 +0100

    dynamic form growing with buttons clicked

--- src/views/AddRecipe/AddRecipe.js
+++ src/views/AddRecipe/AddRecipe.js
@@ -1,17 +1,94 @@
 import React, { Component } from "react";
 
 import {
-    Box, Grid, Heading, Paragraph, ResponsiveContext
+    Box, Button, Form, FormField, Grid, Heading,
+    ResponsiveContext, Select, TextArea,
 } from 'grommet';
+import { Add } from 'grommet-icons';
+
+//import UNITS from "../../data/Constants";
 
 import burger from "../../images/burger.jpg"
 
 import "./AddRecipe.css";
 
 class AddRecipe extends Component {
-    state = {}
+    constructor(props) {
+		super(props);
+        this.state = {
+            ingredientCount: 1,
+            stepCount: 1,
+        }
+    }
+
+    addIngredient = () => {
+        console.log("addIngredient clicked");
+        this.setState({
+            ingredientCount: this.state.ingredientCount + 1
+        });
+    }
+    addStep = () => {
+        console.log("addStep clicked");
+        this.setState({
+            stepCount: this.state.stepCount + 1
+        });
+    }
 
 	render() {
+        let ingredients = [];
+        let steps = [];
+
+        for (let i = 0; i < this.state.ingredientCount; i++){
+            const name_name = `ingredient_${i + 1}_name`;
+            const name_amount = `ingredient_${i + 1}_amount`;
+            const name_unit = `ingredient_${i + 1}_unit`;
+
+            ingredients.push(
+                <Box key={i}
+                    direction="row-responsive"
+                >
+                    <FormField
+                        label="Name:"
+                        name={name_name}
+                    />
+                    <FormField
+                        type="number"
+                        label="Amount:"
+                        name={name_amount}
+                    />
+                    <FormField
+                        label="Unit:"
+                        name={name_unit}
+                        component={ Select }
+                        options={[
+                            "Mililiter(s)",
+                            "Liter(s)",
+                            "Gram(s)",
+                            "Kilogram(s)",
+                            "Teaspoon(s)",
+                            "Tablespoon(s)",
+                            "Cup(s)",
+                        ]}
+                    />
+                </Box>
+            );
+        }
+
+        for (var i = 0; i < this.state.stepCount; i++) {
+            const label = `Step ${i + 1}:`
+            const name = `step_${i + 1}_name`
+
+            steps.push(
+                <Box key={i}>
+                    <FormField
+                        component={ TextArea }
+                        label={label}
+                        name={name}
+                    />
+                </Box>
+            );
+        };
+
 		return(
 			<ResponsiveContext.Consumer>
 				{ size => (
@@ -28,15 +105,40 @@ class AddRecipe extends Component {
 							background={`url(${burger})`}
 						>
 							<Heading level='1' alignSelf='center' color="#F8F8F8">
-								Welcome
+								Add your own recipe!
 							</Heading>
 						</Box>
 						{(size === 'large') &&
 							<Grid
 								gridArea="recipe_form"
-								background="red"
 							>
-                                <Paragraph>random text</Paragraph>
+                                <Form>
+                                    <Box>
+                                        <FormField
+                                            label="Recipe name:"
+                                            name="title"
+                                            required={true}
+                                        />
+                                        <FormField
+                                            component={ TextArea }
+                                            label="Recipe description:"
+                                            name="instructions"
+                                            required={true}
+                                        />
+                                    </Box>
+                                    { ingredients }
+                                    <Button
+                                        icon={ <Add /> }
+                                        label="Add ingredient"
+                                        onClick={()=>{this.addIngredient()}}
+                                    />
+                                    { steps }
+                                    <Button
+                                        icon={ <Add /> }
+                                        label="Add step"
+                                        onClick={()=>{this.addStep()}}
+                                    />
+                                </Form>
 							</Grid>
 						}
 					</Grid>
