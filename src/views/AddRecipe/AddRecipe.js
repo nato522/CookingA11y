dce3b9347154e2af17a63d6e46ab5caded24ccc1
Renato Pinto <renatopl@Renatos-MacBook-Pro.local>
2020-01-04 23:24:18 +0100

    sending entire recipe to localStorage

--- src/views/AddRecipe/AddRecipe.js
+++ src/views/AddRecipe/AddRecipe.js
@@ -6,13 +6,18 @@ import {
 } from 'grommet';
 import { Add } from 'grommet-icons';
 
-//import UNITS from "../../data/Constants";
+import UNITS from "../../data/Constants";
 
 import burger from "../../images/burger.jpg"
 
 import "./AddRecipe.css";
 
 let localRecipes = JSON.parse(localStorage.getItem('my_recipes')) || [];
+
+function _getUnitShort(unitLong){
+    const obj = UNITS.find(obj => obj.unitLong === unitLong);
+    return obj.unitShort;
+}
 
 class AddRecipe extends Component {
     constructor(props) {
@@ -35,20 +40,46 @@ class AddRecipe extends Component {
     }
 
     addRecipe = (e) => {
-        console.log(e.value);
+        let new_recipe_steps = [];
+        let new_recipe_ingredients = [];
+
         const data = e.value;
 
-        /**
-         * TODO: handle array of ingredients and array of instructions
-         */
+        for (let index = 0; index < this.state.ingredientCount; index++) {
+            const ingredient_name = `e.target.elements.ingredient_${index+1}_name.value`;
+            const ingredient_amount = `e.target.elements.ingredient_${index+1}_amount.value`;
+            const ingredient_unit_long = `e.target.elements.ingredient_${index+1}_unit.value`;
+            const ingredient_unit_short = _getUnitShort(eval(ingredient_unit_long));
+
+            const ingredient = {
+                name: eval(ingredient_name),
+                measures:{
+                    metric:{
+                        amount: eval(ingredient_amount),
+                        unitShort: ingredient_unit_short,
+                        unitLong: eval(ingredient_unit_long),
+                    }
+                }
+            }
+            new_recipe_ingredients = new_recipe_ingredients.concat(ingredient);
+        }
+
+        for (let index = 0; index < this.state.stepCount; index++) {
+            let step = `e.target.elements.step_${index+1}_description.value`;
+            step = {
+                number: index+1,
+                step: eval(step),
+            }
+            new_recipe_steps = new_recipe_steps.concat(step);
+        }
 
         const new_recipe = {
             recipe:{
                 title: data.title,
                 instructions: data.instructions,
             },
-            ingredients: [],
-            instructions: [],
+            ingredients: new_recipe_ingredients,
+            instructions: new_recipe_steps,
         }
         localRecipes.push(new_recipe);
         localStorage.setItem('my_recipes', JSON.stringify(localRecipes));
@@ -59,26 +90,24 @@ class AddRecipe extends Component {
         let steps = [];
 
         for (let i = 0; i < this.state.ingredientCount; i++){
-            const name_name = `ingredient_${i + 1}_name`;
-            const name_amount = `ingredient_${i + 1}_amount`;
-            const name_unit = `ingredient_${i + 1}_unit`;
-
             ingredients.push(
                 <Box key={i}
                     direction="row-responsive"
                 >
                     <FormField
                         label="Name:"
-                        name={name_name}
+                        name={`ingredient_${i + 1}_name`}
+                        required={true}
                     />
                     <FormField
                         type="number"
                         label="Amount:"
-                        name={name_amount}
+                        name={`ingredient_${i + 1}_amount`}
+                        required={true}
                     />
                     <FormField
                         label="Unit:"
-                        name={name_unit}
+                        name={`ingredient_${i + 1}_unit`}
                         component={ Select }
                         options={[
                             "Mililiter(s)",
@@ -89,21 +118,20 @@ class AddRecipe extends Component {
                             "Tablespoon(s)",
                             "Cup(s)",
                         ]}
+                        required={true}
                     />
                 </Box>
             );
         }
 
         for (var i = 0; i < this.state.stepCount; i++) {
-            const label = `Step ${i + 1}:`
-            const name = `step_${i + 1}_name`
-
             steps.push(
                 <Box key={i}>
                     <FormField
                         component={ TextArea }
-                        label={label}
-                        name={name}
+                        label={`Step ${i + 1}:`}
+                        name={`step_${i + 1}_description`}
+                        required={true}
                     />
                 </Box>
             );
