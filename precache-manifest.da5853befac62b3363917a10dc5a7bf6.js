dea3cb2a63e6d334ba31efebfc363ea51913f0a8
Renato Pinto <renatopl@Renatos-MacBook-Pro.local>
2020-01-16 23:15:21 +0100

    Updates

---
+++ precache-manifest.da5853befac62b3363917a10dc5a7bf6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fcfe1be9b19731c76b15a78d09c22a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b434815bb89b331a9069",
+    "url": "/static/css/main.0378ead2.chunk.css"
+  },
+  {
+    "revision": "1c5a16cb4fde8b1f99f9",
+    "url": "/static/js/2.f01bfeed.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.f01bfeed.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b434815bb89b331a9069",
+    "url": "/static/js/main.d1e228d5.chunk.js"
+  },
+  {
+    "revision": "aeac87bbaa8878b1ed84",
+    "url": "/static/js/runtime-main.30c0f957.js"
+  },
+  {
+    "revision": "86c58261aebf26e8c78b6d65985e397d",
+    "url": "/static/media/burger.86c58261.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CookingA11y/precache-manifest.f2658de14bf0a42b10a1d510238dfcdb.js"
+  "/precache-manifest.da5853befac62b3363917a10dc5a7bf6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CookingA11y/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
